aeaa8135a7a02df49387a9aae5500dfb5a22a0c3
SeyedV2yay <ejarehseyed@gmail.com>
2026-07-27 22:18:14 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,9 @@
-
+export default {
+  async fetch(request) {
+    return new Response("My V2Ray Worker is running!", {
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8"
+      }
+    });
+  }
+}
